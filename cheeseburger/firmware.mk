940667a284d09df85f5b0007ebe86d61960f92b1
xLexip <lexip.ctrl@gmail.com>
2020-12-06 20:50:37 +0100

    Add firmware.mk's
    Source: https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_oneplus-firmware/

---
+++ cheeseburger/firmware.mk
@@ -0,0 +1,18 @@
+# Firmware
+PRODUCT_COPY_FILES += \
+    vendor/oneplus/firmware/cheeseburger/static_nvbk.bin:install/firmware-update/static_nvbk.bin \
+    vendor/oneplus/firmware/cheeseburger/abl.elf:install/firmware-update/abl.elf \
+    vendor/oneplus/firmware/cheeseburger/adspso.bin:install/firmware-update/adspso.bin \
+    vendor/oneplus/firmware/cheeseburger/BTFM.bin:install/firmware-update/BTFM.bin \
+    vendor/oneplus/firmware/cheeseburger/cmnlib.mbn:install/firmware-update/cmnlib.mbn \
+    vendor/oneplus/firmware/cheeseburger/cmnlib64.mbn:install/firmware-update/cmnlib64.mbn \
+    vendor/oneplus/firmware/cheeseburger/devcfg.mbn:install/firmware-update/devcfg.mbn \
+    vendor/oneplus/firmware/cheeseburger/hyp.mbn:install/firmware-update/hyp.mbn \
+    vendor/oneplus/firmware/cheeseburger/keymaster.mbn:install/firmware-update/keymaster.mbn \
+    vendor/oneplus/firmware/cheeseburger/logo.bin:install/firmware-update/logo.bin \
+    vendor/oneplus/firmware/cheeseburger/NON-HLOS.bin:install/firmware-update/NON-HLOS.bin \
+    vendor/oneplus/firmware/cheeseburger/pmic.elf:install/firmware-update/pmic.elf \
+    vendor/oneplus/firmware/cheeseburger/rpm.mbn:install/firmware-update/rpm.mbn \
+    vendor/oneplus/firmware/cheeseburger/tz.mbn:install/firmware-update/tz.mbn \
+    vendor/oneplus/firmware/cheeseburger/xbl.elf:install/firmware-update/xbl.elf
+
---
+++ dumpling/firmware.mk
@@ -0,0 +1,18 @@
+# Firmware
+PRODUCT_COPY_FILES += \
+    vendor/oneplus/firmware/dumpling/static_nvbk.bin:install/firmware-update/static_nvbk.bin \
+    vendor/oneplus/firmware/dumpling/abl.elf:install/firmware-update/abl.elf \
+    vendor/oneplus/firmware/dumpling/adspso.bin:install/firmware-update/adspso.bin \
+    vendor/oneplus/firmware/dumpling/BTFM.bin:install/firmware-update/BTFM.bin \
+    vendor/oneplus/firmware/dumpling/cmnlib.mbn:install/firmware-update/cmnlib.mbn \
+    vendor/oneplus/firmware/dumpling/cmnlib64.mbn:install/firmware-update/cmnlib64.mbn \
+    vendor/oneplus/firmware/dumpling/devcfg.mbn:install/firmware-update/devcfg.mbn \
+    vendor/oneplus/firmware/dumpling/hyp.mbn:install/firmware-update/hyp.mbn \
+    vendor/oneplus/firmware/dumpling/keymaster.mbn:install/firmware-update/keymaster.mbn \
+    vendor/oneplus/firmware/dumpling/logo.bin:install/firmware-update/logo.bin \
+    vendor/oneplus/firmware/dumpling/NON-HLOS.bin:install/firmware-update/NON-HLOS.bin \
+    vendor/oneplus/firmware/dumpling/pmic.elf:install/firmware-update/pmic.elf \
+    vendor/oneplus/firmware/dumpling/rpm.mbn:install/firmware-update/rpm.mbn \
+    vendor/oneplus/firmware/dumpling/tz.mbn:install/firmware-update/tz.mbn \
+    vendor/oneplus/firmware/dumpling/xbl.elf:install/firmware-update/xbl.elf
+
